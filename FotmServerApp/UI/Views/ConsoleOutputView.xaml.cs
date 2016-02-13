95fb86c96e2dd7b543fbd2b71b14ccdcd4ec7913
Andrew Noyes <anoyes@nexcelom.com>
2016-02-12 16:27:50 -0800

    Fixed the ConsoleOutputView missing file.

---
+++ FotmServerApp/UI/Views/ConsoleOutputView.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows.Controls;
+
+namespace FotmServerApp.UI.Views
+{
+    /// <summary>
+    /// Interaction logic for ConsoleOutputView.xaml
+    /// </summary>
+    public partial class ConsoleOutputView : UserControl
+    {
+
+        public ConsoleOutputView()
+        {
+            InitializeComponent();
+        }
+
+        private bool _autoScroll = true;
+        private void ScrollViewer_OnScrollChanged(object sender, ScrollChangedEventArgs e)
+        {
+            if (e.ExtentHeightChange == 0)
+            {
+                _autoScroll = ScrollViewer.VerticalOffset == ScrollViewer.ScrollableHeight;
+            }
+
+            if (_autoScroll && e.ExtentHeightChange != 0)
+            {
+                ScrollViewer.ScrollToVerticalOffset(ScrollViewer.ExtentHeight);
+            }
+        }
+    }
+}
