7abf68a17240022a93927ebbd1bc63b071ab785e
Andrew <andrew@noyes.io>
2016-02-27 14:22:21 -0800

    Added a datasource dialog to write server/api key to config file. Changed from avalon dock to dragablz to match on the mahapps dark theme. added a start button to start the bracket jobs.

--- Fotm.DAL/Util/ConfigUtil.cs
+++ Fotm.DAL/Util/ConfigUtil.cs
@@ -1,4 +1,6 @@
 ﻿using System.Linq;
+using System.Text;
+using System.Xml;
 using System.Xml.Linq;
 
 namespace Fotm.DAL.Util
@@ -10,11 +12,30 @@ namespace Fotm.DAL.Util
     {
         public static string API_Key, SQL_Server;
 
+        // xml keys
+        private static readonly string API_XML = "API_Key";
+        private static readonly string SQL_XML = "SQL_Server";
+        private static readonly string CONFIG_PATH = "..\\..\\..\\Personal.config";
+
         static ConfigUtil()
         {
-            XDocument xdoc = XDocument.Load("..\\..\\..\\Personal.config");
-            API_Key = xdoc.Descendants("API_Key").First().Value;
-            SQL_Server = xdoc.Descendants("SQL_Server").First().Value;
+            XDocument xdoc = XDocument.Load(CONFIG_PATH);
+            API_Key = xdoc.Descendants(API_XML).First().Value;
+            SQL_Server = xdoc.Descendants(SQL_XML).First().Value;
+        }
+
+        public static void SetConfig(string apiKey, string sqlServer)
+        {
+            var xdoc = XDocument.Load(CONFIG_PATH);
+            xdoc.Descendants(API_XML).First().SetValue(apiKey);
+            xdoc.Descendants(SQL_XML).First().SetValue(sqlServer);
+            using (var writer = new XmlTextWriter(CONFIG_PATH, Encoding.UTF8))
+            {
+                xdoc.WriteTo(writer);
+            }
+
+            API_Key = apiKey;
+            SQL_Server = sqlServer;
         }
     }
 }
