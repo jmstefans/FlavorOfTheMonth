45fd83a96324335a2267cb205be7bb9df97b646a
John Stefanski <jstefanski@sandiego.edu>
2016-03-14 21:02:03 -0700

    small change.

--- FlavorOfTheMonth/Controllers/HomeController.cs
+++ FlavorOfTheMonth/Controllers/HomeController.cs
@@ -36,8 +36,8 @@ namespace FlavorOfTheMonth.Controllers
         public ActionResult About()
         {
             ViewBag.Message = "We use a clustering algorithm to analyze the changes in ratings from the players on the leaderboards."
-                + " Based on these differences we can guess who is playing with who and display the data to you.";
-            ViewBag.Message += "Feel free to contact us at contactpandamic@gmail.com";
+                + " Based on these differences we can guess who is playing with who and display the data to you."
+                + " Feel free to contact us at contactpandamic@gmail.com";
             return View();
         }
 
